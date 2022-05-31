66a0a732ca093e8d92de2a28010c575b1ff7b626
Kininari <reid-cole@outlook.com>
2022-05-31 11:38:35 -0400

    updated story

--- components/StorySection.tsx
+++ components/StorySection.tsx
@@ -23,15 +23,17 @@ const StorySection: React.FC = () => {
         <h2 className={styles.heading}>My Story</h2>
         <p>
           I&apos;ve been into technology, especially software, ever since I was a little kid. I
-          remember trying to jailbreak various devices back then just because I thought it was so
-          cool that you could customize devices beyond what was originally intended. I think I
-          succeeded in rooting one of my android tablets, but some devices were unfortunately
-          bricked due to my dumb 5th grader brain. Naturally, as I got older, I drifted towards the
-          path of coding, since it follows that curiosity for software I&apos;ve always had. Now we
-          are here, where I can create my own software through code. I get to make cool web apps
-          that can be useful tools in solving problems. If I ever need somewhere to write notes and
-          save them in the cloud, I can make it. If my friends ever want a personalized site for us
-          to chat together, I can make it. If you want a website, I can make it. 😸✌️
+          remember trying to jailbreak several devices of mine back then just because I thought it
+          was so cool that you could customize devices beyond what was originally intended. I think
+          I succeeded in rooting one of my android tablets, but some devices were unfortunately
+          bricked due to my dumb 5th grader brain. I also spent much of my time using sites like
+          YouTube and Facebook, and I was always curious about how they worked behind the scenes.
+          When it came time for me to choose a career path, coding seemed only natural for me, since
+          it follows that curiosity for software I&apos;ve always had. Now we are here, where I can
+          create my own software through code. I get to make cool web apps that can be useful tools
+          in solving problems. If I ever need somewhere to write notes and save them in the cloud, I
+          can make it. If my friends ever want a personalized site for us to chat together, I can
+          make it. If you want a website, I can make it. 😸✌️
         </p>
       </div>
       <div>
